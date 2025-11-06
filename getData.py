65331fcc3d9db5e68c53d7854717849a85d412a8
Milady Cabrera <cabreramilady@gmail.com>
2025-11-06 11:27:56 -0500

    getData y getPort pero faltan cosas

--- getData.py
+++ getData.py
@@ -1,19 +1,39 @@
-#script python
+#Script description: Get Data (Temp and Hum) from Arduino
 import serial
 import time
 import os
 
-arduino_port = 'comx'
-band_rate = 9600
-ser = serial.Serial(arduino_port,band_rate, timeout=1)
-time.sleep(2)
-while True
-  data=ser.readline().decode('utf-8').rstrip()
-  if data:
-      humidity,temperature=data.split(",")
-      print(f"humedad:{humidity}%, Tempertura: {temperature}°C")
-  time.sleep(1)
-  #rstrip: elimina espacios en blanco
-  #Timeout
+from supabase import create_client, Client
+from getPort import port
 
-  
+url = "https://rkvpmkwrttweigzgxugr.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrdnBta3dydHR3ZWlnemd4dWdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwOTU2MjMsImV4cCI6MjA3NjY3MTYyM30.v1dnOWu1pqrXgAWQ60TLFbo_u_ORs7HX1FOjnDROMS4"
+supabase: Client = create_client(url, key)
+
+arduino_port = port # Don't worry by the port
+baud_rate = 9600 # Baudios
+ser = serial.Serial(arduino_port, baud_rate, timeout=1)
+
+time.sleep(2)
+
+while True:
+    data = ser.readline().decode('utf-8').rstrip()
+    #print(data)
+    if data:
+        try:
+            humidity, temperature = data.split(",")
+            humidity = float(humidity)
+            temperature = float(temperature)
+
+            # --- ENVÍO A SUPABASE ---
+            response = supabase.table("data").insert({
+                "humidity": humidity,
+                "temperature": temperature
+            }).execute()
+
+            print("✅ Datos enviados:", humidity, temperature)
+        except ValueError:
+            print("⚠️ Formato de datos inválido:", data)
+        except Exception as e:
+            print("❌ Error al enviar a Supabase:", e)
+    time.sleep(1)
